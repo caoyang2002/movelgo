fa2e6c0acae14f40f8025bc6bf72c07bf8a83597
simons <reggiesimons2cy@gmail.com>
2024-07-18 17:41:44 +0800

    update cors

--- users-file/server.ts
+++ users-file/server.ts
@@ -5,23 +5,32 @@ import fs from 'fs';
 import path from 'path';
 import crypto from 'crypto';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 const app = express();
 const PORT = 3001;
 const usersDir = path.join(__dirname, './users'); // 指定用户文件夹的存储目录
+const corsOptions = {
+  origin: 'http://localhost:3000', // 允许的请求来源
+  credentials: true // 允许携带认证信息
+};
+
+app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
 // 设置静态文件目录
 app.use(express.static(path.join(__dirname, 'public')));
 
 // 路由处理
-app.get('/user-file', (req: Request, res: Response) => {
+app.get('/', (req: Request, res: Response) => {
+
   console.log("[用户请求] 用户获取文件名")  
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // Middleware to generate hash and create folder if not exists
 app.use(cookieParser());
-app.use((req: Request & { folderName?: string }, res: Response, next) => {
+app.use('/user-file',(req: Request & { folderName?: string }, res: Response, next) => {
   let folderName = req.cookies['userFolder']; // 从 cookie 中获取 folderName
   console.log("[用户请求] 从 cookie 中获取 folderName", folderName)
 
@@ -29,9 +38,11 @@ app.use((req: Request & { folderName?: string }, res: Response, next) => {
     // 如果 cookie 中没有 folderName，则生成新的 folderName
     console.log("[cookie 中没有 folderName] 生成新的 folderName")
     folderName = generateHash();
-    res.cookie('userFolder', folderName, { maxAge: 34560000000, httpOnly: true }); // 设置 cookie，最大保存 400天
+    res.cookie('userFolder', folderName, { maxAge: 60000, httpOnly: false }); // 设置 cookie，最大保存 400天
+    // res.cookie('userFolder', folderName, { maxAge: 34560000000, httpOnly: false }); // 设置 cookie，最大保存 400天
   }
 
+  console.log("[响应头] ",res.getHeaders());
   const folderPath = path.join(usersDir, folderName);
 
   if (!fs.existsSync(folderPath)) {
@@ -42,6 +53,7 @@ app.use((req: Request & { folderName?: string }, res: Response, next) => {
 
   // 将 folderName 附加到请求对象中，以便后续路由处理函数使用
   req.folderName = folderName;
+  res.json({folderName: req.folderName });
   next();
 });
 
