ca7676a019e96dec0c9ee54352d6524ef897ed26
cy12968@163.com <reggiesimons2cy@gmail.com>
2024-07-19 11:03:00 +0800

    update ui: add test bolck

--- src/views/blockchainSidePane.tsx
+++ src/views/blockchainSidePane.tsx
@@ -5,6 +5,12 @@ import { Header, Content } from '@dtinsight/molecule/esm/workbench/sidebar'
 import { IActionBarItemProps } from '@dtinsight/molecule/esm/components'
 import { localize } from '@dtinsight/molecule/esm/i18n/localize'
 import { ISidebarPane } from '@dtinsight/molecule/esm/model'
+// import PostRequestComponent from 'src/ui_movelgo/handleInputChange'
+// import CreateFolder from 'src/ui_movelgo/createFolder'
+import FetchFloderNameByCookie from 'src/TEST/fetchFloderName'
+import CreatePath from 'src/TEST/createPath'
+import useFolderName from 'src/TEST/fetchFolderNameValue'
+// import CreateAndFetchFiles from '@/TEST/createFile'
 
 const Toolbar = molecule.component.Toolbar
 
@@ -19,17 +25,33 @@ export function BlockchainSidePaneView() {
     ]
   }, [])
 
+  const folderName = useFolderName()
+  const address = '0x1'
+
   return (
-    <div className={'mySidePane'}>
+    <>
+      <div className={'mySidePane'}>
+        <Header
+          title={localize('demo.rightSidebar.title', 'Tools')}
+          toolbar={<Toolbar data={renderHeaderToolbar()} />}
+        />
+        <Content>
+          <p style={{ textAlign: 'center' }}>Move on Aptos</p>
+          <h1 className="">Address: {address}</h1>
+        </Content>
+      </div>
       <Header
-        title={localize('demo.rightSidebar.title', 'Tools')}
+        title={localize('demo.rightSidebar.title', 'TEST')}
         toolbar={<Toolbar data={renderHeaderToolbar()} />}
       />
-      <Content>
-        <p style={{ textAlign: 'center' }}>Move on Aptos</p>
-        <h1 className="">Gas</h1>
-      </Content>
-    </div>
+      <CreatePath />
+
+      <h1 className="">Folder Name</h1>
+      <p>{folderName}</p>
+      {/* <CreateAndFetchFile /> */}
+      <FetchFloderNameByCookie />
+      {/* <CreateFolder /> */}
+    </>
   )
 }
 
