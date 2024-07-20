04054ace1831527d420a0f55bfbe68e841d2c97f
caoyang2002 <cy12968@163.com>
2024-07-20 10:21:08 +0800

    feature (users-file ): folder structure to json

--- users-file/utils.ts
+++ users-file/utils.ts
@@ -1,6 +1,12 @@
-import crypto from 'crypto'
+import * as crypto from 'crypto'
+import * as fs from 'fs'
+import * as path from 'path'
 
-// Generate hash function
+const usersBaseDir = path.join(__dirname, './users')
+/**
+ * 生成 md5 哈希
+ * @returns md5 hash, example: 553dc65fec23f58f97c0f37d36f27fc0
+ */
 export function generateHash() {
   const current_time = new Date().getTime().toString()
   const random = Math.random().toString()
@@ -8,4 +14,108 @@ export function generateHash() {
     .createHash('md5')
     .update(current_time + random)
     .digest('hex')
+}
+
+/**
+ * 生成唯一数字
+ */
+export class UniqueNumberGenerator {
+  private static instance: UniqueNumberGenerator
+  private counter: number
+
+  private constructor() {
+    this.counter = 0
+  }
+
+  static getInstance(): UniqueNumberGenerator {
+    if (!UniqueNumberGenerator.instance) {
+      UniqueNumberGenerator.instance = new UniqueNumberGenerator()
+    }
+    return UniqueNumberGenerator.instance
+  }
+
+  generate(): number {
+    const uniqueNumber = this.counter++
+    return uniqueNumber
+  }
+}
+
+// 检查路径是文件还是文件夹
+export enum PathNodeType {
+  File,
+  Folder,
+  Error,
+}
+
+/**
+ * 检查路径的文件类型（File or Folder）
+ * @param path 用户主目录的相对路径, 例如: 553dc65fec23f58f97c0f37d36f27fc0/Readme.md
+ * @returns utils.FileNodeType
+ */
+export const checkPathType = async (thePath: string): Promise<PathNodeType> => {
+  const fullpath = path.join(usersBaseDir, thePath)
+  console.log('[检查] 开始检查:', thePath)
+  try {
+    const stats = await fs.promises.stat(fullpath)
+    if (stats.isFile()) {
+      console.log('[检查] 类型：文件')
+      return PathNodeType.File
+    } else if (stats.isDirectory()) {
+      console.log('[检查] 类型：目录')
+      return PathNodeType.Folder
+    }
+  } catch (err) {
+    console.error('[检查] 类型：错误', err)
+    return PathNodeType.Error
+  }
+}
+
+/**
+ * 获取文件名
+ * @param path 建议为用户主目录路径的相对文件路径, 例如：553dc65fec23f58f97c0f37d36f27fc0/Readme.md
+ * @returns FileName
+ */
+export const getFileName = (path: string) => {
+  const parts = path.split('/')
+  console.log('[获取] 分割文件夹和文件名', parts)
+  const fileName = parts.pop()
+  return fileName
+}
+
+/**
+ * 获取文件信息
+ * @param filePath 用户主目录的相对路径
+ * @returns 文件后缀、内容、创建时间、最后修改时间、大小
+ */
+export async function getFileInfo(filePath) {
+  console.log('[检查] 文件路径', filePath)
+  const fullPath = path.join(usersBaseDir, filePath)
+  try {
+    // 获取文件元数据
+    const stats = await fs.promises.stat(fullPath)
+    // 文件扩展名
+    const fileExtension = path.extname(fullPath)
+    // 最后修改时间
+    const lastModifiedTime = stats.mtime
+    // 创建时间
+    const createAt = stats.birthtime
+    // 文件大小
+    const fileSize = stats.size
+    // 文件内容
+    const fileContent = await fs.promises.readFile(fullPath, 'utf-8')
+
+    // 返回文件信息
+    return {
+      stats: stats,
+      extension: fileExtension,
+      lastModifiedTime: lastModifiedTime,
+      createAt: createAt,
+      fileSize: fileSize,
+      content: fileContent,
+    }
+  } catch (error) {
+    // 处理错误
+    console.error('Error:', error)
+    throw error
+  }
 }
